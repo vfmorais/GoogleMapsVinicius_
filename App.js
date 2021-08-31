82b64af5cbecbac7bb0e932dba66de0280138304
vfmorais <69993739+vfmorais@users.noreply.github.com>
2021-08-31 17:46:41 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import { View, StyleSheet } from 'react-native';
+export default class App extends Component {
+ render() {
+ return (
+ <View style={styles.container}>
+ 
+ </View>
+ );
+ }
+}
+const styles = StyleSheet.create({
+ container: {
+  flex: 1,
+  justifyContent: 'flex-end',
+  alignItems: 'flex-end',
+ },
+ 
+});
